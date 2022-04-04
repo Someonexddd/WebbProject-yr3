a6e54801ef08ce4f06c50d1957815a00c5b2cbd7
Someonexddd <50412783+Someonexddd@users.noreply.github.com>
2022-04-04 21:38:24 +0200

    added model and DbContext

---
+++ WebbProjekt-yr3/Data/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebbProjekt_yr3.Models;
+
+namespace WebbProjekt_yr3.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+        public DbSet<ProductModel> Product { get; set; }
+    }
+}

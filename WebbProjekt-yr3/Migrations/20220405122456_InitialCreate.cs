77a01da50059e445aff6ed85fd7868c8a3fae91d
Theo Jonsson <0309joth@jonkoping.se>
2022-04-05 14:38:16 +0200

    fungerar auto products

---
+++ WebbProjekt-yr3/Migrations/20220405122456_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebbProjekt_yr3.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseYear = table.Column<DateTime>(type: "date", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Format = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- WebbProjekt-yr3/Models/ProductModel.cs
+++ WebbProjekt-yr3/Models/ProductModel.cs
@@ -6,17 +6,23 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace WebbProjekt_yr3.Models
 {       
     public class ProductModel
     {   
+        [Key]
         public Guid ProductId { get; set; }
+        [Column(TypeName ="nvarchar(max)")]
         public string Name { get; set; }
         [Column(TypeName ="date")]
         public DateTime ReleaseYear { get; set; }
+        [Column(TypeName = "nvarchar(max)")]
         public string Country { get; set; }
+        [Column(TypeName = "nvarchar(max)")]
         public string Format { get; set; }
+        [Column(TypeName = "nvarchar(max)")]
         public string Genre { get; set; }
     }
 }

368fed4ee0e51a88f7e31ee70f758ce2f586fefe
Someonexddd <50412783+Someonexddd@users.noreply.github.com>
2022-04-07 05:00:19 +0200

    Got a workin controller for basic api requests

---
+++ WebbProjekt-yr3/ClientApp/src/components/ProductList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Product from './Product'
+
+
+
+export class ProductList extends ProductList {
+    static displayName = ProductList.name;
+    render() {
+        return(
+            <Product></Product>
+        );
+    }
+}
+
+
+
---
+++ WebbProjekt-yr3/Migrations/20220407014854_ControllerAdded.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebbProjekt_yr3.Migrations
+{
+    public partial class ControllerAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
